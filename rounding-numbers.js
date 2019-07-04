6aa184f8a9d9b64ef2dd7a649f2ff15283363bfc
Amanda Shalansky <ashalansky20@hotmail.com>
2019-07-04 01:50:34 +0000

    Initial commit with all javascripting work so far

---
+++ rounding-numbers.js
@@ -0,0 +1,6 @@
+var roundUp;
+var roundUp = 1.5;
+Math.round(0.5);
+var rounded;
+var rounded = Math.round(roundUp);
+console.log(rounded);
---
+++ strings.js
@@ -0,0 +1,2 @@
+console.log('this is a string');
+console.log('someString');
